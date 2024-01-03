ce37ef2d9569ab50e35f438b6b87e9dbfd0aa53d
Eduard Kaverinskyi <KaverinskyiEduard1813@gmail.com>
2024-01-03 21:44:47 +0100

    Add basic implementation

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import flet as ft
+from modules.todo_list import TodoList
 
 def main(page: ft.Page):
-    page.add(ft.Text(value="Hello, world!"))
+    page.title = "ToDo App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
 
-ft.app(target=main)
+    # create application instance
+    todo = TodoList()
+
+    # add application's root control to the page
+    page.add(todo)
+
+if __name__ == '__main__':
+    ft.app(target=main)
