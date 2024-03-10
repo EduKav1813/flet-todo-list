5a64b750c64eb8e05d151298f658673f8b32d59a
Eduard Kaverinskyi <KaverinskyiEduard1813@gmail.com>
2024-03-10 14:31:02 +0100

    Implement the execute() method for the Database, which will execute given session_processor queries

--- data/repository/tasks_repository.py
+++ data/repository/tasks_repository.py
@@ -1,7 +1,7 @@
 from enum import Enum
-from typing import List
+from typing import Iterable, List
 
-from data.db.database import Database, Session, TasksTable
+from data.db.database import Database, TasksTable
 from data.db.mappers import task_to_tasktable_mapper, tasktable_to_task_mapper
 from entity.task import Task
 
@@ -17,40 +17,46 @@ class TasksRepository:
         self.database = Database()
 
     def insert(self, task: Task) -> None:
-        with Session(self.database.engine) as session:
+        def session_processor(session) -> None:
             session.add(task_to_tasktable_mapper(task))
-            session.commit()
+
+        self.database.execute(session_processor)
 
     def update(self, task: Task) -> None:
-        with Session(self.database.engine) as session:
+        def session_processor(session) -> None:
             task_orm = session.query(TasksTable).where(TasksTable.id == task.id).one()
             task_orm.name = task.name
             task_orm.description = task.description
             task_orm.completed = task.completed
-            session.commit()
+
+        self.database.execute(session_processor)
 
     def update_all(self, tasks: List[Task]) -> None:
-        with Session(self.database.engine) as session:
+        def session_processor(session) -> None:
             if session.query(TasksTable).count() > 0:
                 session.query(TasksTable).delete()
 
             for task in tasks:
                 session.add(task_to_tasktable_mapper(task))
-            session.commit()
+
+        self.database.execute(session_processor)
 
     def get_by_id(self, task_id: int) -> Task:
-        with Session(self.database.engine) as session:
+        def session_processor(session) -> Iterable:
             task = session.query(TasksTable).where(TasksTable.id == task_id).one()
             return map(tasktable_to_task_mapper, task)
 
+        return self.database.execute(session_processor)
+
     def delete_by_id(self, task_id: int) -> None:
-        with Session(self.database.engine) as session:
+        def session_processor(session) -> None:
             task = session.query(TasksTable).get(task_id)
             session.delete(task)
-            session.commit()
+
+        self.database.execute(session_processor)
 
     def get_all_by_status(self, status=TaskStatus) -> List[Task]:
-        with Session(self.database.engine) as session:
+        def session_processor(session) -> Iterable:
             tasks = []
 
             if status == TaskStatus.ALL:
@@ -65,3 +71,5 @@ class TasksRepository:
                 )
 
             return map(tasktable_to_task_mapper, tasks)
+
+        return self.database.execute(session_processor)
